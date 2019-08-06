d3cb2e709752e98fd6bbb8410cd88e0b7616cb25
Alberto Vena <kennyadsl@gmail.com>
2019-08-06 10:43:36 +0200

    Run rubocop -a

--- lib/test_gem_release_gh.rb
+++ lib/test_gem_release_gh.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "test_gem_release_gh/version"
 
 module TestGemReleaseGh
--- spec/test_gem_release_gh_spec.rb
+++ spec/test_gem_release_gh_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe TestGemReleaseGh do
   it "has a version number" do
     expect(TestGemReleaseGh::VERSION).not_to be nil
